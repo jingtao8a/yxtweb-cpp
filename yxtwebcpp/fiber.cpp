e7db343bb36a399f5606411cef494f4e4852fac9
jingtao8a <1921056015@qq.com>
2025-05-29 07:36:32 +0000

    style: add some comment for fiber

--- yxtwebcpp/fiber.cpp
+++ yxtwebcpp/fiber.cpp
@@ -13,6 +13,20 @@
 #include "macro.hpp"
 #include "scheduler.hpp"
 
+
+
+// int swapcontext(ucontext_t *oucp, const ucontext_t *ucp);
+// oucp: 指向当前上下文将被保存的 ucontext_t 结构体
+// ucp: 指向要切换到的上下文 ucontext_t 结构体
+
+// int getcontext(ucontext_t *ucp);
+// ucp: 指向用于存储当前上下文的 ucontext_t 结构体
+
+// void makecontext(ucontext_t *ucp, void (*func)(), int argc, ...);
+// func: 新上下文中要执行的函数
+// argc: 传递给函数的参数个数
+// ...: 实际参数列表（均为 int 类型)
+
 namespace YXTWebCpp {
 
 static std::shared_ptr<Logger> g_logger = YXTWebCpp_LOG_NAME("system");
@@ -50,9 +64,9 @@ Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool use_caller)
     m_context.uc_stack.ss_sp = m_stack;
     m_context.uc_stack.ss_size = m_stacksize;
     m_context.uc_stack.ss_flags = 0;
-    if (!use_caller) {
+    if (!use_caller) { // 设置子协程入口执行函数
         makecontext(&m_context, &Fiber::MainFunc, 0);
-    } else {
+    } else { // 设置主协程入口函数
         makecontext(&m_context, &Fiber::CallMainFunc, 0);
     }
     YXTWebCpp_LOG_DEBUG(g_logger) << "Fiber::Fiber id = " << m_id;
@@ -130,7 +144,7 @@ std::shared_ptr<Fiber> Fiber::GetThis() {//返回当前线程运行的协程Fibe
         t_threadFiber.reset(new Fiber);//给主协程创建一个Fiber对象，并用t_fiber指向它
         SetThis(t_threadFiber.get());
     }
-    return t_fiber->shared_from_this();//返回主协程Fiber对象
+    return t_fiber->shared_from_this();
 }
 
 void Fiber::YieldToHold() {//将当前协程设置为HOLD,当前协程 -> 主协程
@@ -143,13 +157,13 @@ void Fiber::YieldToHold() {//将当前协程设置为HOLD,当前协程 -> 主协
     }
 }
 
-void Fiber::MainFunc() {//执行完成返回到线程主协程
+void Fiber::MainFunc() {// usecaller == false
     Fiber* raw_ptr = FunHelper();
-    raw_ptr->swapOut();//注意协程使用的栈实际上是在堆上开辟的空间
+    raw_ptr->swapOut();
     YXTWebCpp_ASSERT2(false, "never reach")
 }
 
-void Fiber::CallMainFunc() {
+void Fiber::CallMainFunc() {// usecaller == true
     Fiber* raw_ptr = FunHelper();
     raw_ptr->back();
     YXTWebCpp_ASSERT2(false, "never reach");
